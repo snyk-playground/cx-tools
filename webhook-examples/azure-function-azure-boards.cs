ea925e6d677849816bbd8502dd520e3c41de78e8
Harry Kimpel <harrykimpel@hotmail.com>
2022-12-07 13:56:59 +0100

    Enhancing the Azure Boards integration
    Signed-off-by: Harry Kimpel <harrykimpel@hotmail.com>

--- webhook-examples/azure-function-azure-boards.cs
+++ webhook-examples/azure-function-azure-boards.cs
@@ -49,6 +49,7 @@ public static async Task<IActionResult> Run(HttpRequest req, ILogger log)
             string descr = data.newIssues[i].issueData.description.ToString();
             descr = descr.Replace("\"", "'");
             //log.LogInformation("data.newIssues[i].issueData.description:" + descr);
+            string snykOrgid = data.org.id.ToString();
 
             sb.Append("  {");
             sb.Append("    \"op\": \"add\",");
@@ -76,6 +77,33 @@ public static async Task<IActionResult> Run(HttpRequest req, ILogger log)
             var AZURE_DEVOPS_USER = Environment.GetEnvironmentVariable("AZURE_DEVOPS_USER");
             var AZURE_DEVOPS_PAT = Environment.GetEnvironmentVariable("AZURE_DEVOPS_PAT");
             var AZURE_DEVOPS_API_VERSION = Environment.GetEnvironmentVariable("AZURE_DEVOPS_API_VERSION");
+            var AZURE_DEVOPS_SNYKORG_BOARDS_MAPPING = Environment.GetEnvironmentVariable("AZURE_DEVOPS_SNYKORG_BOARDS_MAPPING");
+            var AZURE_DEVOPS_SNYKORG_BOARDS_MAPPING_ENABLED = Boolean.Parse(Environment.GetEnvironmentVariable("AZURE_DEVOPS_SNYKORG_BOARDS_MAPPING_ENABLED"));
+            //log.LogInformation("AZURE_DEVOPS_SNYKORG_BOARDS_MAPPING: " + AZURE_DEVOPS_SNYKORG_BOARDS_MAPPING);
+            //log.LogInformation("AZURE_DEVOPS_SNYKORG_BOARDS_MAPPING_ENABLED: " + AZURE_DEVOPS_SNYKORG_BOARDS_MAPPING_ENABLED);
+
+            if (AZURE_DEVOPS_SNYKORG_BOARDS_MAPPING_ENABLED)
+            {
+                dynamic boardsCustomMapping = JsonConvert.DeserializeObject(AZURE_DEVOPS_SNYKORG_BOARDS_MAPPING);
+                JArray resources = (JArray)boardsCustomMapping;
+                //log.LogInformation("snykOrgid: " + snykOrgid);
+                foreach (var boardsCustomMappingItem in resources
+                    .Where(obj => obj["snykOrgId"].Value<string>() == snykOrgid))
+                {
+                    if (boardsCustomMappingItem != null &&
+                        boardsCustomMappingItem["snykOrgId"] != null)
+                    {
+                        string boardsCustomMappingSnykOrgId = boardsCustomMappingItem["snykOrgId"].ToString();
+                        string boardsCustomMappingAzureBoardsProject = boardsCustomMappingItem["azureBoardsProject"].ToString();
+                        log.LogInformation("boardsCustomMapping.snykOrgId: " + boardsCustomMappingSnykOrgId);
+                        log.LogInformation("boardsCustomMapping.azureBoardsProject: " + boardsCustomMappingAzureBoardsProject);
+
+                        AzureDevOpsProject = boardsCustomMappingAzureBoardsProject;
+                    }
+                }
+
+            }
+
             var url = "https://dev.azure.com/" + AZURE_DEVOPS_ORG + "/" + AzureDevOpsProject + "/_apis/wit/workitems/$Issue?api-version=" + AZURE_DEVOPS_API_VERSION;
             using var client = new HttpClient();
             var authToken = Encoding.ASCII.GetBytes(AZURE_DEVOPS_USER + ":" + AZURE_DEVOPS_PAT);
