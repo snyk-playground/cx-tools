2fc92d483d9a860ae7424a4cf2e7a88fc80010a9
olegshprengelsnyk <oleg.shprengel@snyk.io>
2023-07-17 10:27:13 -0700

    updated non-scripts to new projects endpoint

--- tests/delete-from-repo.sh
+++ tests/delete-from-repo.sh
@@ -1,24 +1,13 @@
-# How to delete all projects of a repo
-
-## Description:
-Works only for CLI. For SCM need to change for browserUrl instead of repoUrl
-
-
-## Instructions
-call : ./your-script-file <API-Token> <RepoName> <org-id>
-
-
-```
 set +x
 APItoken=$1
 repoUrl=$2
 org=$3
 
- projs=($(curl -s \
+ proj1=($(curl -s \
      --request GET \
      --header "Content-Type: application/json" \
      --header "Authorization: ${APItoken}" \
-"https://api.snyk.io/rest/orgs/${org}/projects" | jq --raw-output ' .projects[].id, .projects[].remoteRepoUrl, .projects[].name' ))
+"https://snyk.io/api/v1/org/${org}/projects" | jq --raw-output ' .projects[].id, .projects[].remoteRepoUrl, .projects[].name' ))
 total=${#projs[@]}
 let total=total/3
 
@@ -31,8 +20,8 @@ do
     #echo "${projs[$iii]}" "\t" "${projs[$ii]}" "\t" "${projs[$i]}"   ;
     # Group ID - #org ID - ORG-name
    if  [ "${projs[$ii]}" = "$repoUrl" ] ; then
-      #echo "----------------"
-      #echo "Project\tName: ${projs[$iii]}" "\tRepo: " "${projs[$ii]}" "\tID :" "${projs[$i]}"   ;
+    #   echo "----------------"
+    #   echo "Project\tName: ${projs[$iii]}" "\tRepo: " "${projs[$ii]}" "\tID :" "${projs[$i]}"   ;
 
       curl --include \
            --request DELETE \
