883e2c861a2fe4d32088cdbc004e0001bf3cf90a
olegshprengelsnyk <oleg.shprengel@snyk.io>
2023-09-26 15:27:28 -0600

    added before/after param, as well as ignore-keys

--- bulk-delete/snyk-bulk-delete.py
+++ bulk-delete/snyk-bulk-delete.py
@@ -2,8 +2,11 @@ import sys, getopt, os, snyk
 from yaspin import yaspin
 from helperFunctions import *
 import time
+from datetime import datetime
 
-helpString ='''--help : Returns this page \n--force : By default this script will perform a dry run, add this flag to actually apply changes\n--delete : By default this script will deactivate projects, add this flag to delete active projects instead \n--delete-non-active-projects : By default this script will deactivate projects, add this flag to delete non-active projects instead (if this flag is present only non-active projects will be deleted) \n--origins : Defines origin types of projects to delete\n--orgs : A set of orgs upon which to perform delete,be sure to use org slug instead of org display name (use ! for all orgs)\n--scatypes : Defines SCA type/s of projects to deletes \n--products : Defines product/s types of projects to delete\n--delete-empty-orgs : This will delete all orgs that do not have any projects in them \n* Please replace spaces with dashes(-) when entering orgs \n* If entering multiple values use the following format: "value-1 value-2 value-3"
+
+
+helpString ='''--help : Returns this page \n--force : By default this script will perform a dry run, add this flag to actually apply changes\n--delete : By default this script will deactivate projects, add this flag to delete active projects instead \n--delete-non-active-projects : By default this script will deactivate projects, add this flag to delete non-active projects instead (if this flag is present only non-active projects will be deleted) \n--origins : Defines origin types of projects to delete\n--orgs : A set of orgs upon which to perform delete,be sure to use org slug instead of org display name (use ! for all orgs)\n--scatypes : Defines SCA type/s of projects to deletes \n--products : Defines product/s types of projects to delete\n--delete-empty-orgs : This will delete all orgs that do not have any projects in them \n* Please replace spaces with dashes(-) when entering orgs \n* If entering multiple values use the following format: "value-1 value-2 value-3" \n--after : Only delete projects that were created after a certain date time (in ISO 8601 format, i.e 2023-09-01T00:00:00.000Z)\n--after : Only delete projects that were created before a certain date time (in ISO 8601 format, i.e 2023-09-01T00:00:00.000Z)\n--ignore-keys : An array of key's, if any of these key's are present in a project name then that project will not be targeted for deletion/deactivation
             '''
 
 #get all user orgs and verify snyk API token
@@ -14,6 +17,32 @@ try:
 except snyk.errors.SnykHTTPError as err:
     print("💥 Ran into an error while fetching account details, please check your API token")
     print( helpString)
+def is_date_between(curr_date_str, before_date_str, after_date_str):
+    # Parse the current date string into a datetime object
+    curr_date = datetime.strptime(curr_date_str, '%Y-%m-%dT%H:%M:%S.%fZ')
+
+    # Parse the before date string into a datetime object if it's not empty
+    if before_date_str:
+        before_date = datetime.strptime(before_date_str, '%Y-%m-%dT%H:%M:%S.%fZ')
+    else:
+        before_date = None
+
+    # Parse the after date string into a datetime object if it's not empty
+    if after_date_str:
+        after_date = datetime.strptime(after_date_str, '%Y-%m-%dT%H:%M:%S.%fZ')
+    else:
+        after_date = None
+
+    # Check if the current date is between the before and after dates
+    if before_date and after_date:
+        return curr_date <= before_date and curr_date >= after_date
+    elif before_date:
+        return curr_date <= before_date
+    elif after_date:
+        return curr_date >= after_date
+    else:
+        # If both before and after dates are empty, return True
+        return True
 
 def main(argv):
     inputOrgs = []
@@ -24,11 +53,14 @@ def main(argv):
     dryrun = True
     deactivate = True
     deleteNonActive = False
+    beforeDate = ""
+    afterDate = ""
+    ignoreKeys = []
 
     
     #valid input arguments declared here
     try:
-        opts, args = getopt.getopt(argv, "hofd",["help", "orgs=", "sca-types=", "products=", "origins=", "force", "delete-empty-orgs", "delete", "delete-non-active-projects"] )
+        opts, args = getopt.getopt(argv, "hofd",["help", "orgs=", "sca-types=", "products=", "origins=", "ignore-keys=", "before=","after=", "force", "delete-empty-orgs", "delete", "delete-non-active-projects"] )
     except getopt.GetoptError:
         print("Error parsing input, please check your syntax")
         sys.exit(2)
@@ -46,7 +78,6 @@ def main(argv):
         if opt == '--sca-types':
             scaTypes = [scaType.lower() for scaType in arg.split()]
         if opt == '--products':
-            print(arg)
             products =[product.lower() for product in arg.split()]
         if opt == '--origins':
             origins =[origin.lower() for origin in arg.split()]
@@ -59,6 +90,12 @@ def main(argv):
         if opt =='--delete-non-active-projects':
             deactivate = False
             deleteNonActive = True
+        if opt =='--before':
+            beforeDate = arg
+        if opt =='--after':
+            afterDate = arg
+        if opt =='--ignore-keys':
+            ignoreKeys =[key.lower() for key in arg.split()]
     #error handling if no filters declared
     filtersEmpty = len(scaTypes) == 0 and len(products) == 0 and len(origins) == 0
     if filtersEmpty and not deleteorgs:
@@ -74,7 +111,7 @@ def main(argv):
     
     #print dryrun message
     if dryrun:
-        print("\033[93mTHIS IS A DRY RUN NOTHING, WILL BE DELETED! USE --FORCE TO APPLY ACTIONS\u001b[0m")
+        print("\033[93mTHIS IS A DRY RUN, NOTHING WILL BE DELETED! USE --FORCE TO APPLY ACTIONS\u001b[0m")
     
     #delete functionality
     for currOrg in userOrgs:
@@ -89,13 +126,26 @@ def main(argv):
             #cycle through all projects in current org and delete projects that match filter
             for currProject in currOrg.projects.all():
 
+    
                 #variables which determine whether project matches criteria to delete, if criteria is empty they will be defined as true
                 scaTypeMatch = False
                 originMatch = False
                 productMatch = False
+                dateMatch = False
+                nameMatch = True
                 isActive = currProject.isMonitored
-                
 
+                #dateMatch validation
+                try:
+                    dateMatch = is_date_between(currProject.created, beforeDate, afterDate)
+                except:
+                    print("error processing before/after datetimes, please check your format")
+                    sys.exit(2)
+                #nameMatch validation
+                for key in ignoreKeys:
+                    if key in currProject.name:
+                        nameMatch = False
+                
                 #if scatypes are not declared or curr project type matches filter criteria then return true
                 if len(scaTypes) != 0:
                     if currProject.type in scaTypes:
@@ -119,7 +169,7 @@ def main(argv):
                     productMatch = True  
                 
                 #delete active project if filter are meet
-                if scaTypeMatch and originMatch and productMatch and isActive and not filtersEmpty and not deleteNonActive:
+                if scaTypeMatch and originMatch and productMatch and isActive and not filtersEmpty and not deleteNonActive and dateMatch and nameMatch:
                     currProjectDetails = f"Origin: {currProject.origin}, Type: {currProject.type}, Product: {currProjectProductType}"
                     action =  "Deactivating" if deactivate else "Deleting"
                     spinner = yaspin(text=f"{action}\033[1;32m {currProject.name}", color="yellow")
@@ -135,7 +185,7 @@ def main(argv):
                     except exception as e:
                         spinner.fail("💥 ")
                 #delete non-active project if filters are meet
-                if scaTypeMatch and originMatch and productMatch and (not isActive) and deleteNonActive and not filtersEmpty:
+                if scaTypeMatch and originMatch and productMatch and (not isActive) and deleteNonActive and not filtersEmpty and dateMatch and nameMatch:
                     currProjectDetails = f"Origin: {currProject.origin}, Type: {currProject.type}, Product: {currProjectProductType}"
                     spinner = yaspin(text=f"Deleting\033[1;32m {currProject.name}", color="yellow")
                     spinner.write(f"\u001b[0m    Processing project: \u001b[34m{currProjectDetails}\u001b[0m, Status Below👇")
