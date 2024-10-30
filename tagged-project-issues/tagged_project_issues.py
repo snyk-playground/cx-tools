836fe376b0344e0563f57a0f98d962c4ec167d55
Kevin Matthews <44195753+CSMonkee@users.noreply.github.com>
2024-10-30 12:00:36 +0000

    Auth token was being read from the env. Apply consistent practice to setting it in the env if passed as an optional argument.

--- tagged-project-issues/tagged_project_issues.py
+++ tagged-project-issues/tagged_project_issues.py
@@ -5,7 +5,7 @@ from utils.util_func import tagged_project_issues
 def get_arguments():
     parser = argparse.ArgumentParser(description='This script enables you to configure IQ Server from JSON\
      data, thus supporting the config-as-code requirement of Sonatype customers')
-    parser.add_argument('-a', '--snyk_token', required=True)
+    parser.add_argument('-a', '--snyk_token', default=None)
     parser.add_argument('-g', '--grp_name', required=True)
     parser.add_argument('-o', '--org_name', required=True)
     parser.add_argument('-t', '--project_tags', required=True)
@@ -13,13 +13,14 @@ def get_arguments():
     parser.add_argument('-s', '--effective_severity_level', default="critical,high")
 
     args = vars(parser.parse_args())
+    if args["snyk_token"]:
+        os.environ["SNYK_TOKEN"] = args["snyk_token"]
     return args
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
     args = get_arguments()
-    os.environ['SNYK_TOKEN'] = args['snyk_token']
 
     headers = {
       'Content-Type': 'application/vnd.api+json',
